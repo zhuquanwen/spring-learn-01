39b1ad3de192f2d5af702334902bf2b8b9f86d17
zhuquanwen <12345678a>
2021-09-25 20:24:20 +0800

    11

--- src/main/java/com/spring/learn/beans/support/BeanDefinitionReader.java
+++ src/main/java/com/spring/learn/beans/support/BeanDefinitionReader.java
@@ -2,12 +2,12 @@ package com.spring.learn.beans.support;
 
 import com.spring.learn.Constants;
 import com.spring.learn.config.BeanDefinition;
+import com.spring.learn.util.ScannerUtils;
 
 import javax.servlet.ServletConfig;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  *
@@ -19,6 +19,7 @@ import java.util.Properties;
 public class BeanDefinitionReader implements Constants {
     private String[] locations;
     private Properties contextConfig = new Properties();
+    private Set<Class<?>> registryBeanClass = new HashSet<>();
 
     public BeanDefinitionReader(String[] locations) {
         this.locations = locations;
@@ -27,18 +28,33 @@ public class BeanDefinitionReader implements Constants {
         doLoadConfig(locations[0]);
 
         //2. 解析配置文件
+        try {
+            doScanner(contextConfig.getProperty("scan.packages"));
+        } catch (IOException e) {
+            throw new RuntimeException("解析配置文件出错", e);
+        }
     }
 
     public List<BeanDefinition> doLoadBeanDefinitions() {
-        return null;
+        List<BeanDefinition> definitions = new ArrayList<>();
+        for (Class<?> beanClass : registryBeanClass) {
+            
+        }
+        return definitions;
     }
 
     private void doLoadConfig(String location) {
-        try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(location)) {
+        try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(location.replace("classpath:", ""))) {
             contextConfig.load(is);
         } catch (IOException e) {
             e.printStackTrace();
             //todo 写日志
+        }
+    }
+
+    private void doScanner(String scanPackages) throws IOException {
+        for (String scanPackage : scanPackages.split(",")) {
+            registryBeanClass.addAll(ScannerUtils.getClasses(scanPackage));
         }
     }
 }
